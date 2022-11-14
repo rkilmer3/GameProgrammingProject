a3a85b436eeefd93915afc1770c97f882fad8ff3
Rosa Kilmer <jkilmer3@kent.edu>
2022-11-13 20:27:02 -0500

    Iteration 11/13/22
    Not exactly ready for iteration 3 yet, but this is all that's new for now. Next iteration to implement tomorrow.

---
+++ SonicRecreation/Assets/My Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    public bool isGrounded = true;
+    public float jumpHeight = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded) 
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+            rb.AddForce(movement * speed);
+        }
+
+        if (!isGrounded)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+            rb.AddForce(movement * 0.2f *speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+
+}
