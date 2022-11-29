092032f20e7149760fecdeb88671715e0dec6097
Rosa Kilmer <jkilmer3@kent.edu>
2022-11-29 01:00:24 -0500

    Iteration 4
    Setting up the rest of the game essentially. Added in rings (the first object with full animations) and an enemy which will cause Sonic's rings to reduce to zero if he is not jumping. If he does jump, it should destroy the enemy. To do still, make rings fly out for recollection, figure out how to get Sonic to fly back so that way he's not awkwardly in the enemy, and overall add in the rest of the project.

--- SonicRecreation/Assets/My Scripts/PlayerMovement.cs
+++ SonicRecreation/Assets/My Scripts/PlayerMovement.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    public float speed;
+    public float acceleration;
     public bool isGrounded = true;
     public float jumpHeight = 5;
+    public float currentSpeed;
+    public bool isDead = false;
+    int ringCount;
+    public TMP_Text ringText;
+    public bool rolling;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ringCount = 0;
+        RingText();
     }
 
     // Update is called once per frame
@@ -23,14 +31,14 @@ public class PlayerMovement : MonoBehaviour
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-            rb.AddForce(movement * speed);
+            rb.AddForce(movement * acceleration);
         }
 
         if (!isGrounded)
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-            rb.AddForce(movement * 0.2f *speed);
+            rb.AddForce(movement * 0.2f *acceleration);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1))
@@ -38,18 +46,47 @@ public class PlayerMovement : MonoBehaviour
             if (isGrounded)
             {
                 rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                rolling = true;
             }
         }
+
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
         isGrounded = true;
+        rolling = false;
     }
     void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Rings"))
+        {
+            other.gameObject.SetActive(false);
+            ringCount += 1;
+            RingText();
+        }
+        if (other.CompareTag("Enemy"))
+        {
+            if (rolling)
+            {
+                other.gameObject.SetActive(false);
+                rb.AddForce(Vector2.up * 0.3f, ForceMode2D.Impulse);
+            }
+            else
+            {
+                ringCount -= ringCount;
+                RingText();
+            }
+        }
+    }
 
+    public void RingText()
+    {
+        ringText.text = "Rings:" + ringCount.ToString();
+    }
 }
